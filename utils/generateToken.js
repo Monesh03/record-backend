263b711e45e9857649b1b4380e96e6063df255c1
Monesh03 <moneshs2703@gmail.com>
2025-11-14 02:19:21 +0530

    ios

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -3,17 +3,20 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (res, userUid) => {
   const token = jwt.sign(
-    { userId: userUid },           
+    { userId: userUid },
     process.env.JWT_SECRET,
     { expiresIn: "30d" }
   );
 
- res.cookie("jwt", token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production", 
-  sameSite: "none", 
-  maxAge: 30 * 24 * 60 * 60 * 1000,
-});
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: true,       // Always true on Render
+    sameSite: "none",   // REQUIRED for cross-site cookies
+    path: "/",          // REQUIRED for iOS/Safari/Chrome on iOS
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+
+  return token;
 };
 
 export default generateToken;
