b2f785ec518eb6fdec82e398e4e8c98710a476a6
Monesh03 <151899354+Monesh03@users.noreply.github.com>
2025-11-13 20:49:11 +0530

    updated cors

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // Allow CRA origin (http://localhost:3000)
 app.use(
   cors({
-    origin: "https://record-backend-eight.vercel.app/",
+    origin: "http://localhost:3000/",
     credentials: true, // important for cookie-based JWT
   })
 );
